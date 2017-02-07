126b79219e220015092125f9e7be9829ac4e91f1
Aaron Mann <aaronmann613348@gmail.com>
2017-02-07 09:53:39 -0600

    first commit

---
+++ local2/Placed.swift
@@ -0,0 +1,16 @@
+//
+//  Place.swift
+//  local2
+//
+//  Created by Aaron Mann on 1/12/17.
+//  Copyright © 2017 Aaron Mann. All rights reserved.
+//
+
+import Foundation
+
+
+struct Placed {
+    var latitude : Double
+    var longitude : Double
+    var message : String
+}
